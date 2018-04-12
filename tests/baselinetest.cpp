810035200fd9149b604df96d810887ab1c523047
Matthew James Harrison <mjharry@localhost.localdomain>
2018-04-11 22:43:15 -0400

    baseline test actually uses the baseline function....

--- tests/baselinetest.cpp
+++ tests/baselinetest.cpp
@@ -1,5 +1,8 @@
 #include "gtest/gtest.h"
+#include "baseline.h"
 
-TEST(baseline, test) {
-    ASSERT_EQ(0, 0);
+TEST(baseline, one) {
+    int expected = 1;
+    int actual = baseline_function(1);
+    ASSERT_EQ(expected, actual);
 }
