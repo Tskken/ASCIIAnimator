f410145e52b5c9773af9e878f0c17ae6e9113d71
Matthew James Harrison <mjharry@localhost.localdomain>
2018-04-12 20:20:29 -0400

    implemented, needs tests though

--- src/frame.cpp
+++ src/frame.cpp
@@ -1,10 +1,20 @@
 #include "frame.h"
 
-Frame::Frame(std::string frame_path) {}
+Frame::Frame(std::string path, int index)
+{
+    m_path = path;
+    m_index = index;
+
+    std::fstream fin;
+    fin.open(path, std::fstream::in);
+    std::stringstream buf;
+    buf << fin.rdbuf();
+    m_data = buf.str();
+}
 
 Frame::~Frame() {}
 
 std::string Frame::get_data()
 {
-    return "";
+    return m_data;
 }
--- src/framefetcher.cpp
+++ src/framefetcher.cpp
@@ -1,18 +1,47 @@
 #include "framefetcher.h"
 
-FrameFetcher::FrameFetcher() {}
+FrameFetcher::FrameFetcher()
+{
+    std::thread worker(&FrameFetcher::load_frames, this);
+    worker.join();
+}
 
 FrameFetcher::~FrameFetcher() {}
 
+void FrameFetcher::load_frames()
+{
+    for (int i = 0; i < m_frame_paths.size(); i++)
+    {
+        std::string path = m_frame_paths[i];
+        m_frames.push_back(Frame(path, i));
+    }
+}
+
 int FrameFetcher::get_count()
 {
-    return 0;
+    return m_frame_paths.size();
 }
 
 Frame FrameFetcher::get_frame(int index)
 {
-    Frame frame("");
-    return frame;
+    // Check for out-of-bounds access
+    if (index > m_frame_paths.size())
+    {
+        exit(1);
+    }
+    // Wait until frame is buffered
+    while (index >= m_frames.size());
+    return m_frames[index];
 }
 
-void FrameFetcher::load(std::string animation_path) {}
+void FrameFetcher::load(std::string animation_path)
+{
+    struct dirent *ent = NULL;
+    DIR *dir = opendir(animation_path.c_str());
+    while ((ent = readdir(dir)) != NULL)
+    {
+        std::string name = ent->d_name;
+        m_frame_paths.push_back(animation_path + '/' + name);
+    }
+    closedir(dir);
+}
